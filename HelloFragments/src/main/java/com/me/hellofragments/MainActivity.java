62bcffa2e7a55213ec28812abe539e7060ffd18b
Mohammad Hewedy <mohammed_a_hewedy@hotmail.com>
2013-06-14 04:07:45 +0200

    Show Recent fragment on Activity Create

--- HelloFragments/src/main/java/com/me/hellofragments/MainActivity.java
+++ HelloFragments/src/main/java/com/me/hellofragments/MainActivity.java
@@ -1,15 +1,33 @@
 package com.me.hellofragments;
 
 import android.os.Bundle;
-import android.app.Activity;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentTransaction;
 import android.view.Menu;
+import android.widget.FrameLayout;
 
-public class MainActivity extends Activity {
+public class MainActivity extends FragmentActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FrameLayout frameLayout = (FrameLayout) findViewById(R.id.fragment_container);
+
+        if (frameLayout != null){
+
+            if (savedInstanceState != null){
+                return;
+            }
+
+            RecentFragment recentFragment = new RecentFragment();
+            recentFragment.setArguments(getIntent().getExtras());
+
+            FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+            fragmentTransaction.add(R.id.fragment_container, recentFragment);
+            fragmentTransaction.commit();
+        }
     }
 
 
